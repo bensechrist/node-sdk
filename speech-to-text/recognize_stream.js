841eb4f1ac27a2fe469454d4f1fabfedee1b80ea
Ben Sechrist <bdsechri@us.ibm.com>
2017-01-13 14:03:24 -0600

    Allow sending firmup_silence_time parameter

--- speech-to-text/recognize_stream.js
+++ speech-to-text/recognize_stream.js
@@ -26,7 +26,7 @@ var W3CWebSocket = require('websocket').w3cwebsocket;
 
 var OPENING_MESSAGE_PARAMS_ALLOWED = ['continuous', 'max_alternatives', 'timestamps', 'word_confidence', 'inactivity_timeout',
   'content-type', 'interim_results', 'keywords', 'keywords_threshold', 'word_alternatives_threshold', 'profanity_filter',
-  'smart_formatting', 'speaker_labels' ];
+  'smart_formatting', 'speaker_labels', 'firmup_silence_time' ];
 
 var QUERY_PARAMS_ALLOWED = ['model', 'X-Watson-Learning-Opt-Out', 'watson-token', 'customization_id'];
 
